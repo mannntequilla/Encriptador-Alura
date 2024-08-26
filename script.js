e320801678deac3e4a543a93789d030b05110cf5
Valeria Marin <valmmarin@gmail.com>
2024-08-25 23:22:53 -0400

    Agregar estilizaciones

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 const input = document.querySelector(".input");
 const output = document.querySelector(".output-encriptado");
+const mensaje = document.querySelector(".mensaje");
+const loader = document.querySelector(".loader");
+
 
 function asignarTexto(id, texto) {
     let elementoHTML = document.getElementById(id);
@@ -12,10 +15,31 @@ asignarTexto('titulo', 'Ningún mensaje fue encontrado');
 asignarTexto('parrafo', 'Ingresa el texto que desees encriptar o desencriptar.')
 
 
+let typingTimer; // Timer para detectar el fin de la escritura
+let muneco = document.querySelector(".muneco");
+let tituloDefault = document.querySelector("h2");
+let parrafoDefault = document.querySelector("p");
+
+const typingInterval = 1000; // Tiempo en milisegundos para considerar que ha dejado de escribir
+
+input.addEventListener('input', function() {
+    clearTimeout(typingTimer); // Reinicia el temporizador
+    loader.style.display = 'block'; // Muestra el loader
+    muneco.style.display = "none";
+    tituloDefault.style.display= "none";
+    parrafoDefault.style.display= "none";
+
+   
+    // Configura el temporizador para ocultar el loader si el usuario deja de tipear
+    typingTimer = setTimeout(() => {
+        loader.style.display = "";
+       
+    }, typingInterval);
+});
+
 
 function encriptarMensaje(){
     const textoEncriptado = encriptar(input.value);
-    const mensaje = document.querySelector(".mensaje");
     output.value = textoEncriptado
     input.value="";
     mensaje.style.display = "none"  
@@ -33,7 +57,7 @@ stringEncriptada = stringEncriptada.toLowerCase();
     return stringEncriptada
 }
 
-
+// Funcion para desencriptar
 function desencriptar(stringDesencriptada){
     let matriz = [["e", "enter"],["i","imes"], ["a","ai"], ["o", "ober"], ["u","ufat"]];
     stringDesencriptada = stringDesencriptada.toLowerCase();
@@ -51,6 +75,7 @@ function desencriptar(stringDesencriptada){
     function desencriptarMensaje(){
         const textoEncriptado = desencriptar(input.value)
         output.value = textoEncriptado;
+        input.value = "";
             
     }
 
@@ -64,7 +89,7 @@ function desencriptar(stringDesencriptada){
         // Intentar usar la API moderna del portapapeles
         navigator.clipboard.writeText(output.value)
             .then(() => {
-                alert('Texto copiado al portapapeles');
+                console.log('Texto copiado');
             })
             .catch(err => {
                 console.error('Error al copiar el texto', err);
@@ -77,3 +102,16 @@ function desencriptar(stringDesencriptada){
         fallbackCopy(output);
     }
 }
+
+// Función para recargar la página
+function reiniciarJuego() {
+    location.reload(); // Recarga la página
+}
+
+// Añadir el event listener al logo cuando el DOM esté completamente cargado
+document.addEventListener('DOMContentLoaded', function() {
+    let logo = document.getElementById("logo");
+    logo.addEventListener("click", reiniciarJuego);
+});
+
+
